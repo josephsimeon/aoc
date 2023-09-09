dafa0a67ddf2ac10c04f073abb4b19ce8c13b65c
josephsimeon <40751634+josephsimeon@users.noreply.github.com>
2023-09-09 23:01:22 +1000

    Update day_1.c

--- 2016/day_1.c
+++ 2016/day_1.c
@@ -15,7 +15,7 @@ typedef enum {
     West,
 } Compass_e;
 
-const char puzzle_input[] = "R3, L5, R2, L2, R1, L3, R1, R3, L4, R3, L1, L1, R1, L3, R2, L3, L2, R1, R1, L1, R4, L1, L4, R3, L2, L2, R1, L1, R5, R4, R2, L5, L2, R5, R5, L2, R3, R1, R1, L3, R1, L4, L4, L190, L5, L2, R4, L5, R4, R5, L4, R1, R2, L5, R50, L2, R1, R73, R1, L2, R191, R2, L4, R1, L5, L5, R5, L3, L5, L4, R4, R5, L4, R4, R4, R5, L2, L5, R3, L4, L4, L5, R2, R2, R2, R4, L3, R4, R5, L3, R5, L2, R3, L1, R2, R2, L3, L1, R5, L3, L5, R2, R4, R1, L1, L5, R3, R2, L3, L4, L5, L1, R3, L5, L2, R2, L3, L4, L1, R1, R4, R2, R2, R4, R2, R2, L3, L3, L4, R4, L4, L4, R1, L4, L4, R1, L2, R5, R2, R3, R3, L2, L5, R3, L3, R5, L2, R3, R2, L4, L3, L1, R2, L2, L3, L5, R3, L1, L3, L4, L3"
+const char puzzle_input[] = "R3, L5, R2, L2, R1, L3, R1, R3, L4, R3, L1, L1, R1, L3, R2, L3, L2, R1, R1, L1, R4, L1, L4, R3, L2, L2, R1, L1, R5, R4, R2, L5, L2, R5, R5, L2, R3, R1, R1, L3, R1, L4, L4, L190, L5, L2, R4, L5, R4, R5, L4, R1, R2, L5, R50, L2, R1, R73, R1, L2, R191, R2, L4, R1, L5, L5, R5, L3, L5, L4, R4, R5, L4, R4, R4, R5, L2, L5, R3, L4, L4, L5, R2, R2, R2, R4, L3, R4, R5, L3, R5, L2, R3, L1, R2, R2, L3, L1, R5, L3, L5, R2, R4, R1, L1, L5, R3, R2, L3, L4, L5, L1, R3, L5, L2, R2, L3, L4, L1, R1, R4, R2, R2, R4, R2, R2, L3, L3, L4, R4, L4, L4, R1, L4, L4, R1, L2, R5, R2, R3, R3, L2, L5, R3, L3, R5, L2, R3, R2, L4, L3, L1, R2, L2, L3, L5, R3, L1, L3, L4, L3";
 
 int DetermineBlocksAwayFromHQ(char *input, int size_of_input);
 int SearchForNextDirection(char *input, int size_of_input, int current_position);
@@ -27,7 +27,7 @@ int CalculateBlocksAwayFromPosition(int x, int y);
 
 int main (void)
 {
-    printf("Easter bunny HQ is %i blocks away.\n", DetermineBlocksAwayFromHQ((char *)practice_input_1, sizeof(practice_input_1)));
+    printf("Easter bunny HQ is %i blocks away.\n", DetermineBlocksAwayFromHQ((char *)puzzle_input, sizeof(puzzle_input)));
 }
 
 int DetermineBlocksAwayFromHQ(char *input, int size_of_input)
