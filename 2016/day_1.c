c1cc4847e096089f0061b2f943d62759bf298e64
josephsimeon <40751634+josephsimeon@users.noreply.github.com>
2023-09-09 10:13:26 +1000

    Update day_1.c

--- 2016/day_1.c
+++ 2016/day_1.c
@@ -1,1 +1,8 @@
+#include <stdio.h>
+#include <string.h>
 
+const char practice_input_1[] = "R2, R2, R2";
+#define PRACTICE_RESULT_1 (2)
+
+const char practice_input_2[] = "R5, L5, R5, R3";
+#define PRACTICE_RESULT_2 (12)
