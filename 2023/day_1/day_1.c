559bc10c875ca34c991c61eb3bda96e51ecff3fa
josephsimeon <40751634+josephsimeon@users.noreply.github.com>
2023-12-11 20:07:40 +1000

    Added new files for 2023 day 1; Moved day 1 2015 to it's own folder; Added common folder that will hold usable functions

---
+++ 2023/day_1/day_1.c
@@ -0,0 +1,8 @@
+//! @file       day_1.c
+//! @brief
+//!
+//! @author     Joseph Simeon
+//! @date       11/12/2023 19:48
+//! @url        https://github.com/josephsimeon/aoc/2023
+
+#include "../../common/puzzle.h"
---
+++ common/puzzle.c
@@ -0,0 +1,18 @@
+//! @file       puzzle.h
+//! @brief      This file holds common functions regarding retrieval and processing of
+//!             puzzle information in the more generic manor.
+//!
+//! @author     Joseph Simeon
+//! @date       11/12/2023 19:48
+//! @url        https://github.com/josephsimeon/aoc/common
+
+#include "puzzle.h"
+
+/// @brief  get the puzzle file pointer
+/// @param  name_of_puzzle_text_file puzzle text filename
+/// @return file pointer to the puzzle text
+/// @note   this file does not deal with error handling if the file does not exist
+FILE* GetPuzzleFile(char* name_of_puzzle_text_file)
+{
+    return fopen(name_of_puzzle_text_file, "r");
+}
